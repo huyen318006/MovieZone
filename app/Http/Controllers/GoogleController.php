deee9a023506e67deb8ed1b339e60f01bb218f5f
ayakana12 <nguyenhungpro2k6@gmail.com>
2026-06-07 02:31:57 +0700

    Add Google authentication controllers and update dependencies

---
+++ app/Http/Controllers/GoogleController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Socialite\Facades\Socialite;
+
+class GoogleController extends Controller
+{
+    //
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function handleGoogleCallback()
+    {
+        $user = Socialite::driver('google')->user();
+        // Process the user data as needed
+        // For example, you can create or update a user in your database
+        $finduser = User::where('google_id', $user->id)->first();
+        if ($finduser) {
+            Auth::login($finduser);
+            return redirect()->route('/')->with('success', 'Đăng nhập thành công!');
+
+        } else {
+            // Create a new user
+            $newUser = User::updateOrCreate(
+                ['email'=>$user->email],
+                [
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'google_id' => $user->id,
+                    'password' => Hash::make('12345678') // You can set a default password or generate a random one
+                    // You can also store the avatar or other information if needed
+                ]
+            );
+            Auth::login($newUser);
+            return redirect('/')->with('success', 'Đăng ký và đăng nhập thành công!');
+        }
+    }
+}
