1418f6f8acf56c0415617f2c6f3242ea758d7bfe
Huyen318006 <ngochuyen318006@gmail.com>
2026-06-19 06:18:47 +0700

    Update asset file references in manifest and add combo.css to Vite input

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/css/login.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/css/login.css', 'resources/css/booking/combo.css', 'resources/js/app.js'],
             refresh: true,
         }),
         tailwindcss(),
