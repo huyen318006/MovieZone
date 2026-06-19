15bd6b6b7c19cec3b98fbf66e458d94db6b0ef52
Huyen318006 <ngochuyen318006@gmail.com>
2026-06-19 12:23:30 +0700

    chỉnh lại migration

--- database/migrations/2026_06_05_133757_create_showtimes_table.php
+++ database/migrations/2026_06_05_133757_create_showtimes_table.php
@@ -47,6 +47,8 @@ return new class extends Migration
                 'room_id',
                 'start_time'
             ]);
+            $table->text('cancel_reason')->nullable();
+            $table->timestamp('cancelled_at')->nullable();
         });
     }
 
