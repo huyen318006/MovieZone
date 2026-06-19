1dd312b786a9afcb1572be5b9912e785c49bcf0b
Huyen318006 <ngochuyen318006@gmail.com>
2026-06-19 12:22:49 +0700

    tạo model

--- app/Models/Showtime.php
+++ app/Models/Showtime.php
@@ -19,8 +19,14 @@ class Showtime extends Model
         'format',
         'language_type',
         'status',
+        'cancel_reason',
+        'cancelled_at',
     ];
-
+    protected $casts = [
+        'start_time' => 'datetime',
+        'end_time' => 'datetime',
+        'cancelled_at' => 'datetime',
+    ];
     public function movie()
     {
         return $this->belongsTo(Movie::class);
