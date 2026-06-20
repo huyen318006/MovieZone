5782d32b43593cac60f1cfec7bd6dc0c12d1050e
Huyen318006 <ngochuyen318006@gmail.com>
2026-06-20 13:46:47 +0700

    chỉnh lại view

--- resources/views/admin/showtime/update.blade.php
+++ resources/views/admin/showtime/update.blade.php
@@ -23,6 +23,29 @@
         </div>
 
         <div class="card-body">
+            @if(session('error'))
+                <div class="alert alert-danger alert-dismissible fade show mb-3">
+                    {{ session('error') }}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                </div>
+            @endif
+
+            @if(session('success'))
+                <div class="alert alert-success alert-dismissible fade show mb-3">
+                    {{ session('success') }}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                </div>
+            @endif
+
+            @if ($errors->any())
+                <div class="alert alert-danger mb-3">
+                    <ul class="mb-0">
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
             <form action="{{ route('update.showtime', $showtime->id) }}" method="POST">
                 @csrf
 
