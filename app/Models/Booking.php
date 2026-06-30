a12f3fce428f86ae0751cc2aa6edc554033b0156
ThuKieu26 <kieuthithu04042006@gmail.com>
2026-06-30 16:04:21 +0700

    luong_backend

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -54,4 +54,10 @@ class Booking extends Model
     {
         return $this->hasMany(VoucherUsage::class);
     }
+
+    // Quan hệ 1-1: Một đơn hàng có thể có một thông tin hủy đơn
+    public function cancellation()
+    {
+        return $this->hasOne(BookingCancellation::class, 'booking_id');
+    }
 }
