64a9a740382221e20074600d90c14f89868c9022
Huyen318006 <ngochuyen318006@gmail.com>
2026-07-11 15:01:04 +0700

    xóa trường languages_type

--- database/migrations/2026_06_05_133757_create_showtimes_table.php
+++ database/migrations/2026_06_05_133757_create_showtimes_table.php
@@ -31,7 +31,7 @@ return new class extends Migration
 
             $table->dateTime('end_time');
 
-            $table->string('language_type');
+            // $table->string('language_type');
 
             $table->enum('status', [
                 'OPEN',
